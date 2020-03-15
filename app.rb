9b82fe0b842d117f118f240b17c6bdae27c9fb6d
Braden Douglass <braden.douglass@gmail.com>
2020-03-15 16:53:11 -0400

    Create A Post Test For Product Creation.
    Product creation will take some params. HAL is more of a
    serialization/response format and not a request format. Take an initial
    set of params from the API V1 Product route and emit a product.created
    message with relevant params for consumers to create products where
    relevant.

--- app.rb
+++ app.rb
@@ -8,4 +8,16 @@ class App < Hanami::API
   get '/' do
     200
   end
+
+  scope 'api' do
+    scope 'v1' do
+      post '/products' do
+        publisher = ProductPublisher.new
+        publisher.publish('product.created', product: params)
+
+        status(201)
+        json(params, 'application/hal+json')
+      end
+    end
+  end
 end
---
+++ test/request/product_create_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+# typed: false
+
+require_relative '../spec_helper'
+
+class ProductCreateTest < RequestTest
+  describe "POST /api/v1/products" do
+    let(:product_params) do
+      { 
+        name: 'OldGregg',
+        description: 'The Locke Monster',
+        active: 'true'
+      }
+    end
+
+    subject { app.post '/api/v1/products', product_params }
+
+    it 'successfully sends a message to create a new product' do
+      _(subject.status).must_equal 201
+    end
+  end
+end
