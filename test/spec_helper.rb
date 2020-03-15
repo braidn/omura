675e17eb280b3a1c04267c227471b7db7fdaec6b
Braden Douglass <braden.douglass@gmail.com>
2020-03-15 16:38:56 -0400

    Wire Up Unit Tests For Publishers.
    Include publishers in as required files to Minitest along with creating
    a rather small but, isolated UnitTest class to inherent from. For now
    there isn't any needed functionality but, this should be the target for
    helpers that span all the unit tests.

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -6,12 +6,17 @@ Bundler.require :default, :test
 
 require 'minitest/autorun'
 require 'minitest/spec'
+require 'mocha/minitest'
 require 'rack/mock'
 
 require_relative '../app'
+Dir[File.join(__dir__, '..', 'publishers', '*.rb')].each { |file| require file }
 
 class RequestTest < Minitest::Spec
   def app
     Rack::MockRequest.new(App.new)
   end
+end
+
+class UnitTest < Minitest::Spec
 end
