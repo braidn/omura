52397648c62efda1386556f42b187f94fbc882d5
Braden Douglass <braden.douglass@gmail.com>
2020-05-28 18:45:26 -0400

    Pull In Rack Test To Spec Helper.
    Utilize Rack's Builder pattern to grab the first application in the config.ru.
    Use the Rack Test methods to allow tests to have easy access to the running
    app (just for request specs though).

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -8,13 +8,22 @@ require 'minitest/autorun'
 require 'minitest/spec'
 require 'mocha/minitest'
 require 'rack/mock'
+require 'rack/test'
 
-require_relative '../app'
 Dir[File.join(__dir__, '..', 'publishers', '*.rb')].each { |file| require file }
+Dir[File.join(__dir__, '..', 'events', '*.rb')].each { |file| require file }
+Dir[File.join(__dir__, 'support', 'schemas', '*.rb')].each do |file|
+  require file
+end
+
+APP_STACK =
+  Rack::Builder.parse_file(File.join(__dir__, '..', 'config.ru')).first
 
 class RequestTest < Minitest::Spec
+  include Rack::Test::Methods
+
   def app
-    @app ||= Rack::MockRequest.new(App.new)
+    APP_STACK
   end
 end
 
