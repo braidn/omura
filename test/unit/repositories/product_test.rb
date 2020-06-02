7c21e057ae2e3474c38e74cc0df23ff4c93a9477
Braden Douglass <braden.douglass@gmail.com>
2020-06-02 13:09:52 -0400

    Add Links To Repository Tests.
    The repository itself will alter the product response for now. This adds
    the HAL specific links part of the response.

--- test/unit/repositories/product_test.rb
+++ test/unit/repositories/product_test.rb
@@ -9,10 +9,17 @@ class ProductRepositoryTest < UnitTest
 
   describe '#get' do
     let(:product) do
-      { id: '123ME', name: 'Bilbo Baggins Bag-End', description: 'A Hovel' }
+      {
+        id: '123ME',
+        name: 'Bilbo Baggins Bag-End',
+        description: 'A Hovel',
+        active: true
+      }
     end
+    let(:links) { { _links: { self: { href: "/products/#{product[:id]}" } } } }
+
     it 'returns a single product resource using a unique identifier' do
-      _(subject.get(product[:id])).must_equal product
+      _(subject.get(product[:id])).must_equal product.merge(links)
     end
   end
 end
