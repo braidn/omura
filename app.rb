5735ae071123ca9abeb5c3dcccca7074c7cd5996
Braden Douglass <braden.douglass@gmail.com>
2020-08-08 14:04:24 -0400

    Bail On Hanami API.
    Move to Roda. Roda has a much smaller gem file footprint and since this
    really is a testbed for Sorbet, move to something easier to type.
    Musterman in Hanami Web would boot a server when introspecting gems and
    ultimately fail.
    
    Let's see if Roda fairs better?

--- app.rb
+++ app.rb
@@ -2,31 +2,33 @@
 # frozen_string_literal: true
 
 require 'bundler/setup'
-require 'hanami/api'
+require 'roda'
 require 'ulid'
 require_relative './middlewares/body_params_validator'
 
-class App < Hanami::API
-  get '/' do
-    200
-  end
+class App < Roda
+  route do |r|
+    r.root do
+      200
+    end
 
-  scope 'api' do
-    use BodyParamsValidator
-    scope 'v1' do
-      get '/product/:id' do
-        repo = ProductRepository.new
-        product = repo.get(params[:id])
+    r.on 'api' do
+      r.run BodyParamsValidator
+      r.on scope 'v1' do
+        r.get 'product/:id' do
+          repo = ProductRepository.new
+          product = repo.get(params[:id])
 
-        json(product, 'application/hal+json')
-      end
+          json(product, 'application/hal+json')
+        end
 
-      post '/products' do
-        id = ULID.generate
+        r.post 'products' do
+          id = ULID.generate
 
-        status(201)
-        headers['Location'] = "/api/v1/product/#{id}"
-        json(params, 'application/hal+json')
+          status(201)
+          headers['Location'] = "/api/v1/product/#{id}"
+          json(params, 'application/hal+json')
+        end
       end
     end
   end
