5fafbdfad1164937f2b00fb55fe0f619f32729b7
Braden Douglass <braden.douglass@gmail.com>
2020-08-10 21:45:01 -0400

    Consolidate The Resources Module.
    For now drive all of the logic into the ProductRepository for a Stripe
    Product. Utilize the StripeObject super class to correctly flesh out the
    types of our specific Stripe Product.

--- repositories/product.rb
+++ repositories/product.rb
@@ -1,12 +1,42 @@
 # frozen_string_literal: true
 
 # typed: strict
+require "stripe"
+
+class Product < Stripe::StripeObject
+  extend T::Sig
+
+  sig { retunrs(String) }
+  def id
+  end
+
+  sig { returns(String) }
+  def name
+  end
+
+  sig { returns(T::Boolean) }
+  def active
+  end
+
+  sig { returns(T::Hash(Symbol, String)) }
+  def _links
+  end
+end
+
 class ProductRepository
-  include Resources
+  Stripe.api_key = ENV["STRIPE_API_KEY"]
+  extend T::Sig
 
   def get(id)
     products.select { |product| product[:id] == id }.first.tap do |product|
       product[:_links] = {self: {href: "/products/#{product[:id]}"}}
     end
+  end
+
+  private
+
+  sig { returns(T::Array[Product]) }
+  def products
+    Stripe::Product.list.data
   end
 end
--- repositories/resources.rb
+++ repositories/resources.rb
@@ -1,10 +1,5 @@
 # typed: true
-require "stripe"
 
-module Resources
-  Stripe.api_key = ENV["STRIPE_API_KEY"]
 
-  def products
-    Stripe::Product.list.data
-  end
-end
+
+module Resources; end
