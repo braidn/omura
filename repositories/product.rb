e0f46dc105bfc8eafcfe30919fd647bfe293a1a3
Braden Douglass <braden.douglass@gmail.com>
2020-09-07 13:59:58 -0400

    Fix Product Type Def.
    Utilize the Stripe Product object to define the shape of the return over
    the custom implementation that's built here.

--- repositories/product.rb
+++ repositories/product.rb
@@ -3,30 +3,11 @@
 # typed: strict
 require "stripe"
 
-class Product < Stripe::StripeObject
-  extend T::Sig
-
-  sig { retunrs(String) }
-  def id
-  end
-
-  sig { returns(String) }
-  def name
-  end
-
-  sig { returns(T::Boolean) }
-  def active
-  end
-
-  sig { returns(T::Hash(Symbol, String)) }
-  def _links
-  end
-end
-
 class ProductRepository
   Stripe.api_key = ENV["STRIPE_API_KEY"]
   extend T::Sig
 
+  sig { params(id: String).returns(Stripe::Product) }
   def get(id)
     products.select { |product| product[:id] == id }.first.tap do |product|
       product[:_links] = {self: {href: "/products/#{product[:id]}"}}
@@ -35,7 +16,6 @@ class ProductRepository
 
   private
 
-  sig { returns(T::Array[Product]) }
   def products
     Stripe::Product.list.data
   end
